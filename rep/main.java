0b57828f1b7629b6d4f358924a9e6f7afcd57d6d
Greshnick <grisha-kushnir@mail.ru>
2023-09-11 13:32:19 +0300

    oop

--- rep/main.java
+++ rep/main.java
@@ -1,5 +1,23 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+class Car {
+    private:
+    float speed = 0;
+    bool isMoving = false;
+    public:
+    void stop() {
+        isMoving = false;
     }
-}
+    void accelerate(float amount) {
+        speed += amount;
+        isMoving = speed != 0;
+    }
+    void printSpeed() {
+        std::cout << "Speed: " << speed << std::endl;
+    }
+    void printIsMoving() {
+        if (isMoving) {
+            std::cout << "The car is moving" << std::endl;
+        } else {
+            std::cout << "The car is not moving" << std::endl;
+        }
+    }
+};
